ef16b6bb11fcea2b3b1b4e64507d068afc4bece4
akesia <anakesiaagomes@gmail.com>
2023-11-14 19:49:29 -0300

    first commit

---
+++ formula.py
@@ -0,0 +1,8 @@
+# Trabalho: definição de horários de cursos em um evento
+# Objetivo: fazer um programa que diga se é possível agendar os cursos na quantidade de slots disponíveis e, se possível,
+# dizer em que horário devem ser alocados.
+
+# Número de cursos: k
+# Número de slots de tempo: m; 
+# conjunto P: pares de cursos com inscrições em comum.
+# Participantes podem se inscrever em mais de um curso, mas não no mesmo horário
