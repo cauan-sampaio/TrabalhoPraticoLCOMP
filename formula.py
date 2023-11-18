52b9edfee9357c3bc0c349433e21e21b52834558
Ana Késia Araújo Gomes <106550539+AnaKesia@users.noreply.github.com>
2023-11-18 14:04:34 -0300

    Update formula.py

--- formula.py
+++ formula.py
@@ -6,3 +6,8 @@
 # Número de slots de tempo: m; 
 # conjunto P: pares de cursos com inscrições em comum.
 # Participantes podem se inscrever em mais de um curso, mas não no mesmo horário
+
+#Restrições:
+#1. Cada minicurso deve ser ofertado em pelo menos um slot.
+#2. Cada minicurso deve ser ofertado em no máximo um slot.
+#3. Minicursos com inscrições em comum não podem ser ofertados no mesmo slot.
