3c4a36c58f21f8bd616e9ab9b9324fac3f3cfa10
Cauan <76559406+cauan-sampaio@users.noreply.github.com>
2023-11-20 21:07:24 -0300

    Update formula.py
    Oi

--- formula.py
+++ formula.py
@@ -11,3 +11,21 @@
 #1. Cada minicurso deve ser ofertado em pelo menos um slot.
 #2. Cada minicurso deve ser ofertado em no máximo um slot.
 #3. Minicursos com inscrições em comum não podem ser ofertados no mesmo slot.
+#Entrada:
+
+# Minicursos:
+#1 HTML
+#2 PHP
+#3 MySQL
+#4 Swift
+# Slots: 3
+# Pares de minicursos com inscrições em comum:
+#1 2
+#2 3
+#2 4
+#3 4
+#Saída:
+#1 s1
+#2 s3
+#3 s1
+#4 s2
